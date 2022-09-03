521129ae673be6f338ac29bc36bc32c799b6f10d
Szabó Martin <martinhun19951106@gmail.com>
2022-09-03 12:00:21 +0200

    Now we can give the number of players. Then the program deals two cards for the bank and the players

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,4 @@
-#import random
+import random
 
 card_symbols = ['Diamond','Heart','Spades', 'Club']
 card_values = ['2', '3', '4', '5', '6', '7','8','9','10','J','D','K','A']
@@ -10,9 +10,18 @@ def deck_creation(x,y):
             poker_deck.append(i + " " + j)
     return poker_deck
 
+def deal_two(deck):
+    random.shuffle(deck)
+    dealt = []
+    used = []
+    for i in range(0, (num_of_plyrs+1)*2, 2):
+        dealt.append(deck[i:i + 2])
+        used.append(deck[i])
+        used.append(deck[i+1])
+    return dealt, used
 
-print(deck_creation(card_symbols,card_values))
+num_of_plyrs = int(input("Give me the number of players"))
+print(deal_two(deck_creation(card_symbols,card_values)))
 
-'''random_index = random.randint(0,len(letters)-1)
 
-print(letters[random_index])'''
+
