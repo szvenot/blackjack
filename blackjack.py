e50c65f0a83fe71d285a6f7bb240e5e77661624b
Szabó Martin <martinhun19951106@gmail.com>
2022-09-03 11:18:43 +0200

    Symbols and Values lists created for blackjack game

---
+++ blackjack.py
@@ -0,0 +1,10 @@
+#import random
+
+card_symbols = ['Diamond','Heart','Spades', 'Club']
+card_values = ['2', '3', '4', '5', '6', '7','8','9','10','J','D','K','A']
+
+
+
+'''random_index = random.randint(0,len(letters)-1)
+
+print(letters[random_index])'''
