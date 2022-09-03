9c2233142b3f42502fe4c4d0a52b6ec810758ec7
Szabó Martin <martinhun19951106@gmail.com>
2022-09-03 11:20:50 +0200

    Symbols and values combined into a deck

--- blackjack.py
+++ blackjack.py
@@ -3,7 +3,15 @@
 card_symbols = ['Diamond','Heart','Spades', 'Club']
 card_values = ['2', '3', '4', '5', '6', '7','8','9','10','J','D','K','A']
 
+def deck_creation(x,y):
+    poker_deck = []
+    for i in x:
+        for j in y:
+            poker_deck.append(i + " " + j)
+    return poker_deck
 
+
+print(deck_creation(card_symbols,card_values))
 
 '''random_index = random.randint(0,len(letters)-1)
 
