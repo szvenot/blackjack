561ec4b51a73454bff408384f97193de390fea01
Szabó Martin <martinhun19951106@gmail.com>
2022-09-04 13:58:01 +0200

    Now the program deals a third card for the bank and the players

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,7 @@
 import random
+from tkinter import Y
 
-card_symbols = ['Diamond','Heart','Spades', 'Club']
+card_symbols = ['Diamond','Heart','Spades','Club']
 card_values = ['2', '3', '4', '5', '6', '7','8','9','10','J','D','K','A']
 
 def deck_creation(x,y):
@@ -12,16 +13,36 @@ def deck_creation(x,y):
 
 def deal_two(deck):
     random.shuffle(deck)
-    dealt = []
-    used = []
+    global dealt
+    dealthere = []
+    dealt = dealthere
+    global used
+    usedhere = []
+    used = usedhere
     for i in range(0, (num_of_plyrs+1)*2, 2):
         dealt.append(deck[i:i + 2])
-        used.append(deck[i])
-        used.append(deck[i+1])
-    return dealt, used
+        usedhere.append(deck[i])
+        usedhere.append(deck[i+1])
+    return dealt
 
+ 
 num_of_plyrs = int(input("Give me the number of players"))
-print(deal_two(deck_creation(card_symbols,card_values)))
+deal = deal_two(deck_creation(card_symbols, card_values))
+print(deal)
 
+def hit(j,deck):
+    if_fold = input("Do you wanr a hit?")
+    if if_fold == "y":
+        random.shuffle(deck)
+        for i in range(1):
+            j.append(deck[i])
+            print(j)
+    elif if_fold == "n":
+        return j
+    else:
+        print("Quiet joking")
+
+for pairs in dealt:
+    hit(pairs,deck_creation(card_symbols, card_values))
 
 
